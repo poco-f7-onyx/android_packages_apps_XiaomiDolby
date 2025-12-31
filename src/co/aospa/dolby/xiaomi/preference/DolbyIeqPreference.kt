ca1458afe792365e222099936eb490eedbcbdb06
basamaryan <basam.aryan@gmail.com>
2025-12-31 10:31:09 +0100

    dolby: Fix IEQ icon crash
    Return a default icon to prevent crashes when uninitialized
    
    Change-Id: I0c65d195b57ab261ae6fe404b210d28676ad626b

--- src/co/aospa/dolby/xiaomi/preference/DolbyIeqPreference.kt
+++ src/co/aospa/dolby/xiaomi/preference/DolbyIeqPreference.kt
@@ -39,7 +39,7 @@ class DolbyIeqPreference(
             1 -> R.drawable.ic_ieq_balanced
             2 -> R.drawable.ic_ieq_warm
             3 -> R.drawable.ic_ieq_detailed
-            else -> 0 // should never hit this!
+            else -> R.drawable.ic_ieq_off
         }
     }
 }
